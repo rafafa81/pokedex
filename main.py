55f797996137cd1a6fd36e26f0a51efb2d146bbf
rafafa81 <rafafa81@hotmail.com>
2021-03-02 23:06:46 -0600

    more readable
    modifications to make the script more easy to understand

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import requests
 import json
+
 #variables
 pokeNumber=1
 response_json={'name':None}
+
 #loop to retrieve the information of every pokemon
 ### important - delete the limiter pokeNumber of the while condition 
 while response_json['name'] != "" and pokeNumber <= 10:
@@ -10,12 +12,16 @@ while response_json['name'] != "" and pokeNumber <= 10:
     response=requests.get(httpsURL)
     if response.status_code==200 :
         response_json=json.loads(response.text)
+
         #below information will be changed as needed
         #--------------------------------------------
         print(response_json['name'])
         #--------------------------------------------
+
         pokeNumber=pokeNumber+1
+        
     else:
+        
         #in case the get request fails
         response_json['name']=""
         print("Failed to connect to pokeAPI")
